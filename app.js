d9686001ffe4607865073d28c0501d0da9ae94ef
Bhavesh Bhatia <bhaveshbhatia39@gmail.com>
2024-04-04 18:56:44 +0530

    added null checks

--- app.js
+++ app.js
@@ -1,35 +1,56 @@
 let cartItems=[];
 
+
+
+(function(){
+    cartItems=JSON.parse(localStorage.getItem("cartItems"));
+
+    let checkoutItems = document.getElementById("checkout-items");
+    console.log(checkoutItems);
+    if(checkoutItems){
+            
+        let str="";
+        for(let i=0;i<cartItems.length; i++){
+            str+=`<li> ${cartItems[i].name} , price: ${cartItems[i].price}</li>`;
+        }
+        
+        checkoutItems.innerHTML=str;
+    }git 
+})();
+
+
 // add item from localstorage
 
 function fetchSavedItems(){
 
     cartItems=JSON.parse(localStorage.getItem("cartItems"));
-
-
-    for(let i=0;i<cartItems.length; i++){
-    item=cartItems[i];
-    const cartItem=document.createElement('div');
-    cartItem.classList.add('cart-item','d-flex','justify-content-between','text-capitalize','my-3');
-
-    cartItem.innerHTML=`<img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
-    <div class="cart-item-text">
-
-      <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
-      <span>₹</span>
-      <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
-    </div>
-    <a href="#" id='cart-item-remove' class="cart-item-remove">
-      <i class="fas fa-trash"></i>
-    </a>
-    </div>`;
-    //select cart
     const cart=document.getElementById("cart");
-    const total=document.querySelector(".cart-total-container");
-    cart.insertBefore(cartItem,total);
-    showtotals();
-    }
 
+    if(cart){
+
+        for(let i=0;i<cartItems.length; i++){
+            item=cartItems[i];
+            const cartItem=document.createElement('div');
+            cartItem.classList.add('cart-item','d-flex','justify-content-between','text-capitalize','my-3');
+
+            cartItem.innerHTML=`<img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+            <div class="cart-item-text">
+
+            <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+            <span>₹</span>
+            <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+            </div>
+            <a href="#" id='cart-item-remove' class="cart-item-remove">
+            <i class="fas fa-trash"></i>
+            </a>
+            </div>`;
+            //select cart
+            
+            const total=document.querySelector(".cart-total-container");
+            cart.insertBefore(cartItem,total);
+            showtotals();
+        }
+    }
 }
 
 fetchSavedItems();
@@ -39,45 +60,46 @@ fetchSavedItems();
 (function(){
     const cartInfo = document.getElementById("cart-info");
     const cart = document.getElementById("cart");
-
-    cartInfo.addEventListener("click",function(){
-        cart.classList.toggle("show-cart");
-    });
+    if(cartInfo){
+        cartInfo.addEventListener("click",function(){
+            cart.classList.toggle("show-cart");
+        });
+}
 })();
 
 //clear cart
 (function(){
     const clearCart = document.getElementById("clear-cart");
-    clearCart.addEventListener("click",function(){
-        
-        let cartItems = document.getElementsByClassName("cart-item");
-        console.log(cartItems);
-        cartItems;
-        while(cartItems.length>0){
-            cartItems[0].remove();
-        }
-        cartItems=[];
-        localStorage.setItem("cartItems",JSON.stringify(cartItems));
+    if(clearCart)
+    {   clearCart.addEventListener("click",function(){
+            let cartItems = document.getElementsByClassName("cart-item");
+            console.log(cartItems);
+            cartItems;
+            while(cartItems.length>0){
+                cartItems[0].remove();
+            }
+            cartItems=[];
+            localStorage.setItem("cartItems",JSON.stringify(cartItems));
 
-        document.getElementById('cart-total').textContent = 0;
-        // document.querySelector('.item-total').textContent = 0;
-        // document.getElementById('item-count').textContent = 0;
-        showtotals();
-    });
+            document.getElementById('cart-total').textContent = 0;
+            // document.querySelector('.item-total').textContent = 0;
+            // document.getElementById('item-count').textContent = 0;
+            showtotals();
+        });
+    }
 })();
 
 //delete item from cart
 (function(){
     const delItem = document.querySelectorAll('.cart-item-remove');
-
-    delItem.forEach(function(btn){
-        btn.addEventListener("click",function(event){
-            
-            const dItem= event.target.parentElement.parentElement;
-            console.log(dItem);
-
+    if(delItem){
+        delItem.forEach(function(btn){
+            btn.addEventListener("click",function(event){
+                const dItem= event.target.parentElement.parentElement;
+                console.log(dItem);
+            });
         });
-    });
+    }
 })();
 
 //add item to cart
@@ -85,54 +107,56 @@ fetchSavedItems();
 (function(){
     const cartBtn = document.querySelectorAll('.store-item-icon');
 
-    cartBtn.forEach(function(btn){
-        btn.addEventListener("click",function(event){
-            //console.log(event.target);
+    if(cartBtn){
+        cartBtn.forEach(function(btn){
+            btn.addEventListener("click",function(event){
+                //console.log(event.target);
 
-        if(event.target.parentElement.classList.contains("store-item-icon"))
-        {
-            let fullPath = event.target.parentElement.previousElementSibling.src;
-            let pos = fullPath.indexOf('img')+3;
-            let partPath = fullPath.slice(pos);
+            if(event.target.parentElement.classList.contains("store-item-icon"))
+            {
+                let fullPath = event.target.parentElement.previousElementSibling.src;
+                let pos = fullPath.indexOf('img')+3;
+                let partPath = fullPath.slice(pos);
+                
+                const item={};
+
+                item.img=`img-cart${partPath}`;
+                
+                let name= event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+                item.name=name;
+                let price= event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+                let finalPrice=price.slice(1).trim();
+                item.price=finalPrice;
+
+                //console.log(item);
+                const cartItem=document.createElement('div');
+                cartItem.classList.add('cart-item','d-flex','justify-content-between','text-capitalize','my-3');
+
+                cartItem.innerHTML=`<img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+                <div class="cart-item-text">
+
+                <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                <span>₹</span>
+                <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                </div>
+                <a href="#" id='cart-item-remove' class="cart-item-remove">
+                <i class="fas fa-trash"></i>
+                </a>
+            </div>`;
+            //select cart
+            const cart=document.getElementById("cart");
+            const total=document.querySelector(".cart-total-container");
+
+            cartItems.push(item)
+            localStorage.setItem("cartItems",JSON.stringify(cartItems));
             
-            const item={};
-
-            item.img=`img-cart${partPath}`;
-            
-            let name= event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
-            item.name=name;
-            let price= event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
-            let finalPrice=price.slice(1).trim();
-            item.price=finalPrice;
-
-            //console.log(item);
-            const cartItem=document.createElement('div');
-            cartItem.classList.add('cart-item','d-flex','justify-content-between','text-capitalize','my-3');
-
-            cartItem.innerHTML=`<img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
-            <div class="cart-item-text">
-
-              <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
-              <span>₹</span>
-              <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
-            </div>
-            <a href="#" id='cart-item-remove' class="cart-item-remove">
-              <i class="fas fa-trash"></i>
-            </a>
-          </div>`;
-        //select cart
-        const cart=document.getElementById("cart");
-        const total=document.querySelector(".cart-total-container");
-
-        cartItems.push(item)
-        localStorage.setItem("cartItems",JSON.stringify(cartItems));
-        
-        cart.insertBefore(cartItem,total);
-        alert("item added to cart");
-        showtotals();
-        }
+            cart.insertBefore(cartItem,total);
+            alert("item added to cart");
+            showtotals();
+            }
+            });
         });
-    });
+    }
 })();
 
 
@@ -141,19 +165,20 @@ fetchSavedItems();
 function showtotals(){
     const total=[];
     const items=document.querySelectorAll(".cart-item-price");
-
-    items.forEach(function(item){
-        total.push(parseFloat(item.textContent));
-    });
-    const totalMoney=total.reduce(function(total,item){
-        total+=item;
-        return total;
-    },0);
-    const finalMoney = totalMoney.toFixed(2);
-    
-    document.getElementById('cart-total').textContent = finalMoney;
-    document.querySelector('.item-total').textContent = finalMoney;
-    document.getElementById('item-count').textContent = total.length;
+    if(items){
+        items.forEach(function(item){
+            total.push(parseFloat(item.textContent));
+        });
+        const totalMoney=total.reduce(function(total,item){
+            total+=item;
+            return total;
+        },0);
+        const finalMoney = totalMoney.toFixed(2);
+        
+        document.getElementById('cart-total').textContent = finalMoney;
+        document.querySelector('.item-total').textContent = finalMoney;
+        document.getElementById('item-count').textContent = total.length;
+    }
 }
 
 
@@ -163,7 +188,7 @@ function showtotals(){
     const buttons = document.getElementsByClassName('filter-btn');
     const storeItems= document.querySelectorAll('.store-item');
     //console.log(buttons);
-    Array.from(buttons).forEach((button)=> {
+   if(buttons){ Array.from(buttons).forEach((button)=> {
         button.addEventListener('click', (e) => {
             e.preventDefault();
             const filter = e.target.dataset.filter;
@@ -180,7 +205,7 @@ function showtotals(){
                 }
             })
         })
-    })
+    })}
 })();
 
 //wire up search filter box
@@ -188,19 +213,19 @@ function showtotals(){
 
     const searchBox = document.querySelector('#search-item')
     const storeItems = document.querySelectorAll('.store-item')
+    if(searchBox){
+        searchBox.addEventListener('keyup', (e) => {
+        
+            const searchFilter = e.target.value.toLowerCase().trim()
+            //display only items that contain filter input
 
-    searchBox.addEventListener('keyup', (e) => {
-    
-        const searchFilter = e.target.value.toLowerCase().trim()
-        //display only items that contain filter input
-
-        storeItems.forEach((item) => {
-            if (item.textContent.includes(searchFilter)){
-                item.style.display = 'block'
-            } else {
-                item.style.display = 'none'
-            }
+            storeItems.forEach((item) => {
+                if (item.textContent.includes(searchFilter)){
+                    item.style.display = 'block'
+                } else {
+                    item.style.display = 'none'
+                }
+            })
         })
-    })
-
+    }
 })();
